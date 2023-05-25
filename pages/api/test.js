47650b75b12cfde27b320db1b5d16dbe21c8a361
Ian Langworth <ian@langworth.com>
2023-05-24 22:18:27 -0700

    Add catch

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,8 +1,9 @@
 export default function handler(req, res) {
   setTimeout(() => {
-    fetch("https://ca43-70-252-16-66.ngrok-free.app")
+    fetch("https://ca43-70-252-16-66.ngrok-free.app/response")
       .then((res) => res.text())
-      .then((text) => console.log(text));
+      .then((text) => console.log(text))
+      .catch((err) => console.log(err));
     console.log("test", new Date().toISOString());
   }, 1000);
 
