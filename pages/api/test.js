9e3f35a3e4ffa15298a8d7842cf0b66fd8a0b777
Ian Langworth <ian@langworth.com>
2023-05-24 22:16:36 -0700

    Add fetch

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,10 @@
 export default function handler(req, res) {
   setTimeout(() => {
+    fetch("https://ca43-70-252-16-66.ngrok-free.app")
+      .then((res) => res.text())
+      .then((text) => console.log(text));
     console.log("test", new Date().toISOString());
   }, 1000);
+
   res.status(200).json({ success: new Date().toISOString() });
 }
